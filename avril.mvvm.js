d927ed57fd954045c2297e73b59fec052b06e314
Judd Trump <wangzhenchuan@gmail.com>
2014-06-26 19:24:24 +0800

    begin avril mvvm framework

---
+++ avril.mvvm.js
@@ -0,0 +1,19 @@
+/**
+ * Created by trump.wang on 2014/6/26.
+ */
+;(function(av,$){
+    avril.namespace('avril.mvvm');
+
+    avril.createlib('avril.mvvm.TemplateParser',function(options){
+        var cfg = $.extend(this.options(),{
+                /* default config */
+            } , options)
+            , cache = {};
+
+        this.parse = function(context){
+
+        }
+
+    });
+
+})(avril,jQuery);
