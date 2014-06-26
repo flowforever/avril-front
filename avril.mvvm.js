19f227e986dd09bb05c6a13243c915d8c6a4a76e
Judd Trump <wangzhenchuan@gmail.com>
2014-06-26 19:27:58 +0800

    parser

--- avril.mvvm.js
+++ avril.mvvm.js
@@ -1,7 +1,7 @@
 /**
  * Created by trump.wang on 2014/6/26.
  */
-;(function(av,$){
+;(function(av,$, win){
     avril.namespace('avril.mvvm');
 
     avril.createlib('avril.mvvm.TemplateParser',function(options){
@@ -10,10 +10,10 @@
             } , options)
             , cache = {};
 
-        this.parse = function(context){
+        this.parse = function(dom,controller){
+            controller = controller || win;
 
         }
-
     });
 
-})(avril,jQuery);
+})(avril,jQuery, window);
