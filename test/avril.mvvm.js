e2b43273f0c64b0894877e7d388b9be55c4160e4
Judd Trump <wangzhenchuan@gmail.com>
2014-10-08 23:11:30 +0800

    test case

--- test/avril.mvvm.js
+++ test/avril.mvvm.js
@@ -25,37 +25,49 @@
     };
 
     for(var k in rootModel){
-        avril.mvvm.setVal(k , rootModel[k]);
+        mvvm.setVal(k , rootModel[k]);
     }
 
-    avril.mvvm.bindDom(document);
 
 
-    describe('avril.mvvm.js', function(){
-        describe('#av-bind', function(){
 
-            it('form#form0 input[name=firstName] value should be :'+ rootModel.basicForm.firstName, function(){
-                expect($('input[name=firstName]').val()).equal(rootModel.basicForm.firstName);
-            });
+    $(function(){
 
-            it('form#form0 input[name=fullName] value should be :'+ rootModel.basicForm.firstName +' '+ rootModel.basicForm.lastName, function(){
-                expect($('input[name=fullName]').val()).equal(rootModel.basicForm.firstName+ ' ' + rootModel.basicForm.lastName);
-            });
+        avril.mvvm.bindDom(document);
 
-            it('form#form0 input[name=firstName] value change to :'+ rootModel.basicForm.nFirstName, function(done){
-                setTimeout(function(){
-                    $('input[name=firstName]').val( rootModel.basicForm.nFirstName).trigger('change');
+        describe('avril.mvvm.js', function(){
+
+            this.timeout(100000);
+
+            describe('#av-bind', function(){
+
+                it('form#form0 input[name=firstName] value should be :'+ rootModel.basicForm.firstName, function(){
+                    expect($('input[name=firstName]').val()).equal(rootModel.basicForm.firstName);
+                });
+
+                it('form#form0 input[name=fullName] value should be :'+ rootModel.basicForm.firstName +' '+ rootModel.basicForm.lastName, function(){
+                    expect($('input[name=fullName]').val()).equal(rootModel.basicForm.firstName+ ' ' + rootModel.basicForm.lastName);
+                });
+
+                it('form#form0 input[name=firstName] value change to :'+ rootModel.basicForm.nFirstName, function(done){
+                    setTimeout(function(){
+                        $('input[name=firstName]').val( rootModel.basicForm.nFirstName).trigger('change');
 
 
-                    expect($('input[name=firstName]').val()).equal(rootModel.basicForm.nFirstName);
+                        expect($('input[name=firstName]').val()).equal(rootModel.basicForm.nFirstName);
 
-                    expect($('input[name=fullName]').val()).equal(rootModel.basicForm.nFirstName+ ' ' + rootModel.basicForm.lastName);
+                        expect($('input[name=fullName]').val()).equal(rootModel.basicForm.nFirstName+ ' ' + rootModel.basicForm.lastName);
 
-                    done();
-                },1500)
+                        done();
+                    },2000)
+                })
             })
-        })
+        });
+
     });
+
+
+
 })();
 
 
